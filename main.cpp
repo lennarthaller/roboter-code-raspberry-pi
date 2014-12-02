79a83621ece0b62af294906af62753f69c5c20b8
lennarthaller <post@lennarthaller.de>
2014-12-02 16:31:32 +0100

    Anpassung der Motorkraft zu Testzwecken

--- main.cpp
+++ main.cpp
@@ -22,9 +22,9 @@ int main () {
 		cout << "Fehler" << endl;
 	}
 	
-	cout << "Aktuelle Entfernung zu naechstem Hinderniss: " << g_pSeriell->GetPMLDistance () << endl;
-	cout << "Atueller Kompas Wert: " << g_pWiringPi->GetCompassData() << endl;
-	g_pSeriell->SetMotorPower (1,30);
+	//cout << "Aktuelle Entfernung zu naechstem Hinderniss: " << g_pSeriell->GetPMLDistance () << endl;
+	//cout << "Atueller Kompas Wert: " << g_pWiringPi->GetCompassData() << endl;
+	g_pSeriell->SetMotorPower (1,150);
 	Log_File->Del ();
 	g_pNetwork->Del ();
 	g_pWiringPi->Del ();
