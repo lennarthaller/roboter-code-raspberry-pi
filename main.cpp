bb5003fb15ee2b43dc9eee41917366370551bc33
lennarthaller <post@lennarthaller.de>
2014-12-20 12:47:38 +0100

    2D Scan durchführen und diesen per WLAN verschicken

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include "Network.hpp"
 #include "Logfile.hpp"
 
+#include <unistd.h>
+
 using namespace std;
 
 int main () {
@@ -12,6 +14,8 @@ int main () {
 	cout << "Roboter test Programm v0.1" << endl;
 	cout << "von" << endl << "Lennart Haller" << endl;
 	
+	int nSeriellData[2];
+	int nDaten = 0;
 	//wiringPi init
 	if (g_pWiringPi->InitWiringPi() != 1) {
 		cout << "Fehler" << endl;
@@ -22,14 +26,40 @@ int main () {
 		cout << "Fehler" << endl;
 	}
 	
-	cout << "Aktuelle Entfernung zu naechstem Hinderniss: " << g_pSeriell->GetPMLDistance () << endl;
+	if (g_pNetwork->ConnectToClient () != 1) {
+		cout << "Fehler" << endl;
+	}
+	
+	//cout << "Aktuelle Entfernung zu naechstem Hinderniss: " << g_pSeriell->GetPMLDistance () << endl;
 	//cout << "Atueller Kompas Wert: " << g_pWiringPi->GetCompassData() << endl;
 	//g_pSeriell->SetMotorPower (1,150);
 	
 	cout << "Lichtschranke 1: " << g_pSeriell->GetPhotoSensorData(1) << endl;
-	cout << "Betriebsspannung: " << g_pSeriell->GetBatteryVoltage() << endl;
+	//cout << "Betriebsspannung: " << g_pSeriell->GetBatteryVoltage() << endl;
 	
-	g_pSeriell->MovePML(1);
+	while (1==1) { 
+		//cout << "Berechnet: " << g_pSeriell->GetPMLDistance () << endl;
+		/*g_pWiringPi->SendSeriellInt (6);
+		nSeriellData[0] = g_pWiringPi->ReceiveSeriellData ();
+		nSeriellData[1] = g_pWiringPi->ReceiveSeriellData ();
+	nDaten = (nSeriellData[1] << 8) | nSeriellData[0];
+		cout << "Daten des Sensors: " << nDaten << endl;
+		cout << "-----------" << endl;
+		usleep (500000); */
+		
+		for (int i=0;i<100;i++) {
+		g_pNetwork->Send (g_pSeriell->GetPMLDistance ());
+		g_pSeriell->MovePML (1);
+		usleep (30000);
+		cout << i << endl;
+		}
+		for (int i=0;i<100;i++) {
+		g_pSeriell->MovePML (0);
+		usleep (500);
+		}
+		g_pNetwork->Send (1025);
+	}
+	//g_pSeriell->MovePML(1);
 	
 	Log_File->Del ();
 	g_pNetwork->Del ();
