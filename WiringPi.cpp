a46988643fa883707f4afa41da380a5a486aaf4c
lennarthaller <post@lennarthaller.de>
2014-12-22 22:10:25 +0100

    Rückgabewert der TimeSinceStart Funktion auf Millisekunden/10 angepasst

--- WiringPi.cpp
+++ WiringPi.cpp
@@ -36,5 +36,5 @@ float CWiringPi::GetCompassData () {
 }
 
 clock_t CWiringPi::TimeSinceStart () {
-	return  (clock () / (CLOCKS_PER_SEC/1000));
+	return  (clock () / (CLOCKS_PER_SEC/100)); //gibt die vergangene Zeit seit Start in Millisekunden/10 zurück
 }
