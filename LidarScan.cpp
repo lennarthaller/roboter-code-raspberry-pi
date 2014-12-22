52f9a5ee26013f601cf820935f8f33ccba7ba145
lennarthaller <post@lennarthaller.de>
2014-12-22 23:44:29 +0100

    Kosmetische Änderungen

--- LidarScan.cpp
+++ LidarScan.cpp
@@ -12,25 +12,31 @@ CLidarScan::CLidarScan () {
 
 void CLidarScan::Scan () {
 	if (m_bScanActive == true) {	//Scan durchführen
+		
 		if (m_nTimeStampSinceLastCall + 300 < g_pWiringPi->TimeSinceStart()) { //30 Millisekunden seit dem letzten Aufruf vergangen?
+			
 			if (m_nScanStepCounter < 100) {		//Läuft der Scan noch? (noch keine 100 Schritte)
 				m_nScanData[m_nScanStepCounter] = g_pSeriell->GetInfraredDistance ();
 				std::cout << m_nScanData[m_nScanStepCounter] << std::endl;
 				g_pSeriell->MovePML (1);
 				m_nScanStepCounter ++;
 				m_nTimeStampSinceLastCall = g_pWiringPi->TimeSinceStart();
+			
 			}else{		//Scan fertig
 				m_nScanStepCounter = 0;
 				m_bScanActive = false;
 				m_nTimeStampSinceLastCall = 0;
 			}
 		}
+	
 	}else{	//Zurückdrehen
 		if (m_nTimeStampSinceLastCall + 5 < g_pWiringPi->TimeSinceStart()) { //1 Millisekunde seit dem letzten Aufruf vergangen?
+			
 			if (m_nScanStepCounter < 100) {
 				g_pSeriell->MovePML (0);
 				m_nScanStepCounter ++;
 				m_nTimeStampSinceLastCall = g_pWiringPi->TimeSinceStart();
+			
 			}else{
 				m_nScanStepCounter = 0;
 				m_nTimeStampSinceLastCall = 0;
