d2cf28c42939bbbe661ce429c52fb38a54415a40
lennarthaller <post@lennarthaller.de>
2014-12-25 01:10:04 +0100

    Constructor added to the knowledgebase class

--- KnowledgeBase.cpp
+++ KnowledgeBase.cpp
@@ -1,7 +1,10 @@
 #include "KnowledgeBase.hpp"
 
 CKnowledgeBase::CKnowledgeBase () {
-
+	m_fCalculatedDrivingDirection = 0;
+	for (int i=0;i<100;i++) {
+		m_nScannerData[i] = 0;
+	}
 }
 
 void CKnowledgeBase::SetScannerData (int nScanerData[]) {
