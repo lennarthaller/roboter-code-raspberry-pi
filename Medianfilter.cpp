bb8ffceebc53eefa8fb3d4d14d329de01da58234
lennarthaller <post@lennarthaller.de>
2015-05-15 20:38:59 +0200

    Implemented the Medianfilter class

---
+++ Medianfilter.cpp
@@ -0,0 +1,26 @@
+#include "Medianfilter.hpp"
+
+CMedianfilter::CMedianfilter (int SizeOfFilter, int NumberOfDatapoints) {
+	m_nSizeOfFilter = SizeOfFilter;
+	m_nNumberOfDatapoints = NumberOfDatapoints;
+	if (NumberOfDatapoints > 100) {
+		std::cout << "ZU VIELE DATEN IM MEDIANFILTER!" << std::endl; /////DEBUG
+	}
+}
+
+ void CMedianfilter::FilterData (int Data[]) {
+	 std::vector<int> DataVec (m_nSizeOfFilter);
+	 
+	for (int i=0;i<m_nNumberOfDatapoints; i++) {
+		m_nData[i] = Data[i];
+		m_nFilteredData[i] = Data[i];
+	}
+	for (int i=(m_nSizeOfFilter/2); i<m_nNumberOfDatapoints-(m_nSizeOfFilter/2); i++) {
+		for (int j=0; j<m_nSizeOfFilter; j++) {
+			DataVec.push_back (m_nData[i+j]);
+		}
+		std::sort (DataVec.begin(), DataVec.end());
+		m_nFilteredData[i] = DataVec[(m_nSizeOfFilter/2)+1];
+		DataVec.clear ();
+	}
+}
---
+++ Medianfilter.hpp
@@ -0,0 +1,22 @@
+#ifndef MEDIANFILTER_HPP
+#define MEDIANFILTER_HPP
+
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+class CMedianfilter
+{
+	public:
+	CMedianfilter (int SizeOfFilter, int NumberOfDatapoints);
+	void FilterData (int Data[]);
+	int* GetFilteredData () {return m_nFilteredData;}
+	
+	private:
+	int m_nSizeOfFilter;
+	int m_nNumberOfDatapoints;
+	int m_nData[100]; 
+	int m_nFilteredData[100];
+};
+
+#endif
