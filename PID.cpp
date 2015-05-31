4b8a0152dc923e330f8716aeabdc3e545f090af4
lennarthaller <post@lennarthaller.de>
2015-05-31 22:48:43 +0200

    Fixed some bugs

--- PID.cpp
+++ PID.cpp
@@ -2,9 +2,9 @@
 
 CPidController::CPidController (float fDt, float fP, float fI, float fD) {
 	for (int i=0; i<3; i++) {
-		m_nData[i] = 0;
+		m_nDeviation[i] = 0;
 	}
-	m_fCalculatedValue = 0.of;
+	m_fCalculatedValue = 0.0f;
 	
 	m_fDt = fDt;
 	m_fP = fP;
@@ -12,12 +12,12 @@ CPidController::CPidController (float fDt, float fP, float fI, float fD) {
 	m_fD = fD;
 }
 
-int CPidController::Calculate (int nSetpointValue, int nActualValue) [
-	m_nData[1] = m_nData[0]
-	m_nData[2] = m_nData[1]
-	m_nData[0] = nSetpointValue - nActualValue;
+int CPidController::Calculate (int nSetpointValue, int nActualValue) {
+	m_nDeviation[1] = m_nDeviation[0];
+	m_nDeviation[2] = m_nDeviation[1];
+	m_nDeviation[0] = nSetpointValue - nActualValue;
 	
-	int nCalculatedValue = static_cast<int> ((m_fP * m_nData[0]) + (m_fD * (m_nData[0]-m_nData[1]) / m_fDt) + (m_fI * m_fDt * (m_nData[0]+m_nData[1]+m_nData[2])));
+	int nCalculatedValue = static_cast<int> ((m_fP * m_nDeviation[0]) + (m_fD * (m_nDeviation[0]-m_nDeviation[1]) / m_fDt) + (m_fI * m_fDt * (m_nDeviation[0]+m_nDeviation[1]+m_nDeviation[2])));
 	
 	return nCalculatedValue;
 }
