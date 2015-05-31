5e5d2685fee877b3c6aa566f6c671e7abb282d3a
lennarthaller <post@lennarthaller.de>
2015-05-31 19:07:36 +0200

    Addet and implemented the PID class

---
+++ PID.cpp
@@ -0,0 +1,23 @@
+#include "PID.hpp"
+
+CNetworkProtocol::CNetworkProtocol (float fDt, float fP, float fI, float fD) {
+	for (int i=0; i<3; i++) {
+		m_nData[i] = 0;
+	}
+	m_fCalculatedValue = 0.of;
+	
+	m_fDt = fDt;
+	m_fP = fP;
+	m_fI = fI;
+	m_fD = fD;
+}
+
+int CNetworkProtocol::Calculate (int nSetpointValue, int nActualValue) [
+	m_nData[1] = m_nData[0]
+	m_nData[2] = m_nData[1]
+	m_nData[0] = nSetpointValue - nActualValue;
+	
+	int nCalculatedValue = static_cast<int> ((m_fP * m_nData[0]) + (m_fD * (m_nData[0]-m_nData[1]) / m_fDt) + (m_fI * m_fDt * (m_nData[0]+m_nData[1]+m_nData[2])));
+	
+	return nCalculatedValue;
+}
---
+++ PID.hpp
@@ -0,0 +1,24 @@
+#ifndef PID_HPP
+#define PID_HPP
+
+#include "KnowledgeBase.hpp"
+
+#include <iostream>
+
+class CNetworkProtocol
+{
+public:
+	CNetworkProtocol (float fDt, float fP, float fI, float fD);
+	int Calculate (int nSetpointValue, int nActualValue);
+	
+private:
+	int m_nDeviation[3];
+	float m_fCalculatedValue;
+	
+	float m_fDt;
+	float m_fP;
+	float m_fI;
+	float m_fD;
+};
+
+#endif
