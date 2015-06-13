8eaa6942faa43af1903650f8cb3ab4e606f9b43c
lennarthaller <post@lennarthaller.de>
2015-06-13 21:48:22 +0200

    Addet and implemented the timer class

---
+++ Timer.cpp
@@ -0,0 +1,22 @@
+#include "Timer.hpp"
+
+long CTimer::TimeSinceStart () {
+	timespec Time;
+	if (clock_gettime (CLOCK_REALTIME, &Time) == -1) {
+		std::cout << "ERROR: SYSTEM CLOCK OFFLINE" << std::endl;
+	}	
+	//return (((Time.tv_sec - 1429623010) * 10000) + Time.tv_nsec / 100000);
+	return (((Time.tv_sec - m_nTimerToZero) * 10000) + Time.tv_nsec / 100000);
+}
+
+int CTimer::InitTimer () {
+	Log_File->WriteTopic ("Init system timer", 1);
+	timespec Time;
+	if (clock_gettime (CLOCK_REALTIME, &Time) == -1) {
+		Log_File->Textout (RED, "Failed to initialise the timer!");
+		return -1;
+	}	
+	m_nTimerToZero = Time.tv_sec;
+	Log_File->Textout (BLACK, "Sytem timer initialised.");
+	return 1;
+}
