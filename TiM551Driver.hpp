6147c9acadab37747662806bd45519e4d9dfb259
lennarthaller <post@lennarthaller.de>
2015-09-05 19:49:19 +0200

    SendSOPASCommands is now private; implemted GetData function

--- TiM551Driver.hpp
+++ TiM551Driver.hpp
@@ -2,11 +2,13 @@
 #define TIM551DRIVER_HPP
 
 #include <iostream>
+//#include <libusb-1.0/libusb.h>
 #include <libusb.h>
-/*
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <vector>
+//#include <stdio.h>
+
+/*#include <stdlib.h>
 */
 
 #include "Tracer.hpp"
@@ -16,13 +18,14 @@ class CTiM551Driver
   public:
     int InitLaserScanner ();
     int CloseLaserScanner ();
+    int GetData ();
 
   private:
+   int sendSOPASCommand (const char *request, std::vector<unsigned char> &reply);
 
     // libusb
     libusb_context *ctx;
     ssize_t numberOfDevices;
-    libusb_device **devices;
     libusb_device_handle *device_handle;
 
 };
