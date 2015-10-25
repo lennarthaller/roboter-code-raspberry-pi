58d9e033aa0b14aa01f58b120c2b99c67d02abdf
lennarthaller <post@lennarthaller.de>
2015-10-25 14:54:47 +0100

    Removed an unneeded bracket; compiles now

--- KnowledgeBase.cpp
+++ KnowledgeBase.cpp
@@ -45,6 +45,6 @@ void CKnowledgeBase::SetOdometryTicksSinceLastUpdate (int nOdometryTicks[]) {
 	}
 }
 
-void CKnowledgeBase::SetCurrentMotorPower ((int nMotorNumber, int nCurrentMotorPower) {
+void CKnowledgeBase::SetCurrentMotorPower (int nMotorNumber, int nCurrentMotorPower) {
 	m_nCurrentMotorPower[nMotorNumber-1] = nCurrentMotorPower;
 }
